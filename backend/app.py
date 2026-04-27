b6022c86e15ca49d3923554f19c020919574fd39
Your Name <your-email@example.com>
2026-04-28 01:23:13 +0530

    Frontend + basic backend setup

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)   # ✅ ADD THIS
+
+UPLOAD_FOLDER = "uploads"
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route("/")
+def home():
+    return "Backend Running"
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files["file"]
+    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(filepath)
+
+    return jsonify({"result": "Image uploaded successfully"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
